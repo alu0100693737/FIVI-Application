6351d6cfc1d532d7ca227625ca45fa045655f5bf
alu0100693737 <alu0100693737@ull.edu.es>
2016-01-29 19:43:45 +0100

    incluyendo temas, trabajando ajax y parte voz

---
+++ www/ComprobarTemas.php
@@ -0,0 +1,46 @@
+<?php
+//Cambia por los detalles de tu base datos
+	$dbserver = "mysql.hostinger.es";
+	$dbuser = "u261167719_root";
+	$password = "sistemas";
+	$dbname = "u261167719_temas";
+//    $dbserver = "localhost";
+//	$dbuser = "root";
+//	$password = "";
+//	$dbname = "temas";
+
+
+        $database = new mysqli($dbserver, $dbuser, $password, $dbname);
+
+	if($database->connect_errno) {
+                die("No se pudo conectar a la base de datos");
+	}
+
+	$jsondata = array();
+	
+	if ( $result = $database->query( "SELECT NombreTema FROM temas") ) {
+
+		if( $result->num_rows > 0 ) {
+
+                     
+                     $jsondata["temas"] = array();
+                     while( $row = $result->fetch_object() ) {
+                                $jsondata["temas"][] = $row;
+                     }
+			
+		} 
+				
+		
+		
+		$result->close();
+		
+	} 
+	
+/*convierte los resultados a formato json*/
+$resultadosJson = json_encode($jsondata);
+
+/*muestra el resultado en un formato que no da problemas de seguridad en browsers */
+echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
+//echo $resultadosJson;
+
+?>
