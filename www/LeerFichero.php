2907ec0d5cd90cde304a9e2380135cd350521f56
Estefanía Morales <alu0100698688@ull.edu.es>
2016-05-22 13:01:52 +0100

    Adecuado el fichero porescrito.html para el sensor de geolocalización y modificado LeerFichero.php en el servidor en consecuencia. No funciona envío longitud y latitud

--- www/LeerFichero.php
+++ www/LeerFichero.php
@@ -7,32 +7,54 @@ $nombreFichero = $temaElegido.".txt";
 $preguntaOinformacion= substr($pregunta, -1);
 
 if($preguntaOinformacion != "?"){
-    $fichero = fopen("docs/".$nombreFichero,"a");
-    fputs($fichero, $pregunta."\r\n");
-    $jsondata["message"] = "Información añadida correctamente";
+    if(strcmp ($temaElegido , "Examenes" ) == 0){
+      $fichero = fopen("docs/".$nombreFichero,"a");
+      fputs($fichero, $pregunta."\r\n");
+      $jsondata["message"] = "Información añadida correctamente";
+    }else{
+      $jsondata["message"] = "No se puede añadir información sobre este tema";
+    }
 }else{
     $contenido = explode(" ",$pregunta);
-$preguntaFinal = explode("?",$contenido[count($contenido)-1]);
+    $preguntaFinal = explode("?",$contenido[count($contenido)-1]);
     $fichero = fopen("docs/".$nombreFichero,"r");
     $linea="";
     $encontrada=false;
     while(!feof($fichero) && ($encontrada==false)){
         $linea = fgets($fichero);
-        $encontrada= stripos($linea, $preguntaFinal[0]);  
+        $encontrada= stripos($linea, $preguntaFinal[0]);
     }
     if($encontrada === false){
 
             $jsondata["message"] = "No tengo informacion para contestar a esa pregunta";
-        }else{
+    }else{
 
-
-            $jsondata["message"] = $linea;
+            if(strcmp ($temaElegido , "Examenes" ) == 0){
+              $lineaEnPartes = explode(",",$linea);
+              $latitudFichero = (double)($lineaEnPartes[1]);
+              $longitudFichero = (double)($lineaEnPartes[2]);
+              $geolocalizacion = $_GET['geolocation'];
+              $geolocation = explode(":",$geolocalizacion);
+              $latitud = (double)$geolocation[1];
+              $longitud=(double)$geolocation[3];
+              // $latitud = $_GET["latitud"];
+              // $longitud = $_GET["longitud"];
+              // $latitud = (double)$latitud;
+              // $longitud = (double)$longitud;
+              if((($latitudFichero >= ($latitud-0.5)) && ($latitudFichero <= ($latitud+0.5))) && (($longitudFichero >= ($longitud-0.5)) && ($longitudFichero <= ($longitud+0.5)))){
+                    $jsondata["message"] = $lineaEnPartes[0];
+              }else{
+                    $jsondata["message"] = "Frase: ".$lineaEnPartes[0]." latitud: ".$latitud." longitud: ".$longitud." y nosotros latitudFichero: ".$latitudFichero." y longitudFichero: ".$longitudFichero." No hay exámenes registrados en esta zona";
+              }
+            }else{
+              $jsondata["message"] = $linea;
+            }
     }
 }
 fclose($fichero);
-      
+
 $resultadosJson = json_encode($jsondata);
-//echo $resultadosJson;      
+//echo $resultadosJson;
 echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
-    
+
 ?>
