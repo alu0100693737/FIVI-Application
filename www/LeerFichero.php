2cb0bbe135c1efe57dcafe3887a2d76b64a960ad
Estefanía Morales <alu0100698688@ull.edu.es>
2016-05-25 17:56:51 +0100

    Corregida comprobación en tema Examenes (geolocalización). Ampliado rango de las zonas.

--- www/LeerFichero.php
+++ www/LeerFichero.php
@@ -20,34 +20,45 @@ if($preguntaOinformacion != "?"){
     $fichero = fopen("docs/".$nombreFichero,"r");
     $linea="";
     $encontrada=false;
-    while(!feof($fichero) && ($encontrada==false)){
-        $linea = fgets($fichero);
-        $encontrada= stripos($linea, $preguntaFinal[0]);
-    }
-    if($encontrada === false){
+    if(strcmp ($temaElegido , "Examenes" ) == 0){
+      $latitud = (double)($_GET["latitud"]);
+      $longitud = (double)($_GET["longitud"]);
 
+      while(!feof($fichero) && ($encontrada==false)){
+          $linea = fgets($fichero);
+          $lineaEnPartes = explode(",",$linea);
+          $latitudMaxFichero = (double)($lineaEnPartes[1]);
+          $latitudMinFichero = (double)($lineaEnPartes[2]);
+          $longitudMaxFichero = (double)($lineaEnPartes[3]);
+          $longitudMinFichero = (double)($lineaEnPartes[4]);
+          $examenCorrecto = stripos($linea, $preguntaFinal[0]);
+          if(($examenCorrecto == true)&&(($latitud >= $latitudMinFichero) && ($latitud <= $latitudMaxFichero)) && (($longitud >= $longitudMinFichero) && ($longitud <= $longitudMaxFichero))){
+             $encontrada = true;
+          }
+      }
+    }else{
+      while(!feof($fichero) && ($encontrada==false)){
+          $linea = fgets($fichero);
+          $encontrada = stripos($linea, $preguntaFinal[0]);
+
+      }
+    }
+    if($encontrada === false && $temaElegido == "Examenes"){
+            $jsondata["message"] = "Ese examen no se realiza en el aula en la que se encuentra";
             $jsondata["message"] = "No tengo informacion para contestar a esa pregunta";
     }else{
-
-            if(strcmp ($temaElegido , "Examenes" ) == 0){
-              $lineaEnPartes = explode(",",$linea);
-              $latitudMaxFichero = (double)($lineaEnPartes[1]);
-              $latitudMinFichero = (double)($lineaEnPartes[2]);
-              $longitudMaxFichero = (double)($lineaEnPartes[3]);
-              $longitudMinFichero = (double)($lineaEnPartes[4]);
-              $latitud = (double)($_GET["latitud"]);
-              $longitud = (double)($_GET["longitud"]);
-
-              if((($latitud >= $latitudMinFichero) && ($latitud <= $latitudMaxFichero)) && (($longitud >= $longitudMinFichero) && ($longitud <= $longitudMaxFichero))){
-                    $jsondata["message"] = $lineaEnPartes[0];
-              }else{
-                    $jsondata["message"] = "Ese examen no se realiza en el aula en la que se encuentra";
-              }
-            }else{
-              $jsondata["message"] = $linea;
-            }
+      if($encontrada === false && $temaElegido != "Examenes"){
+        $jsondata["message"] = "No tengo informacion para contestar a esa pregunta";
+      }else{
+        if($encontrada === true && $temaElegido == "Examenes"){
+          $jsondata["message"] = $lineaEnPartes[0];
+        }else{
+          $jsondata["message"] = $linea;
+        }
+      }
     }
-}
+
+  }
 fclose($fichero);
 
 $resultadosJson = json_encode($jsondata);
